6ebf8d26176e93dd0b6fa26d25d19763908b6b87
darlanberg <darlan.cursos@gmail.com>
2024-08-15 17:46:30 -0300

    Verificaçao de usuario no login (defeituoso)

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import json
 
 app = Flask(__name__)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+caminho_dados = 'dados.json'
+
+def ler_dados():
+    with open(caminho_dados, 'r') as dados:
+        informacoes_usuarios = json.load(dados)
+        return informacoes_usuarios
 
 @app.route("/")
 def main():
@@ -14,9 +22,23 @@ def estoque():
     return render_template("estoque.html")
 
 @app.route("/relatorios")
-def estoque():
+def relatorios():
     return render_template("relatorios.html")
 
 @app.route("/cadastrovendas")
-def estoque():
+def cadastro_vendas():
     return render_template("cadastro_vendas.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        senha = request.form["senha"]
+        dados = ler_dados()
+        for usuario in dados:
+            if usuario["email"] == email and usuario["senha"] == senha:
+                return redirect(url_for("relatorios"))
+            else:
+                return ""
+    return render_template("tela_login.html")
+
