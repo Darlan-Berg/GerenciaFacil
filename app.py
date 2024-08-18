d0005133134e1e2cc13ed9e509dd25e39ee8138c
Davi Berg <davibergmarcosdasilva@gmail.com>
2024-08-18 17:57:10 -0300

    Adição da função cadastro de usuários

--- app.py
+++ app.py
@@ -6,12 +6,25 @@ app = Flask(__name__)
 if __name__ == "__main__":
     app.run(debug=True)
 
-caminho_dados = 'dados.json'
+caminho_dados = "dados.json"
 
 def ler_dados():
-    with open(caminho_dados, 'r') as dados:
+    with open(caminho_dados, "r") as dados:
         informacoes_usuarios = json.load(dados)
         return informacoes_usuarios
+
+def adicionar_usuario(nome, cpf, email, senha):
+    novo_usuario = {
+        "nome": nome,
+        "cpf": cpf,
+        "email": email,
+        "senha": senha
+    }
+    novos_dados = ler_dados()
+    novos_dados.append(novo_usuario)
+
+    with open(caminho_dados, "w") as dados:
+        json.dump(novos_dados, dados, indent=4)
 
 @app.route("/")
 def main():
@@ -39,6 +52,23 @@ def login():
             if usuario["email"] == email and usuario["senha"] == senha:
                 return redirect(url_for("relatorios"))
             else:
-                return ""
+                return "<h1>Senha ou Usuário incorretos</h1>"
     return render_template("tela_login.html")
 
+@app.route('/cadastro', methods=["GET", "POST"])
+def cadastro():
+    if request.method == "POST":
+        nome = request.form["nome"]
+        cpf = request.form["cpf"]
+        email = request.form["email"]
+        senha = request.form["senha"]
+        dados = ler_dados()
+        for usuario in dados:
+            if usuario["email"] == email:
+                return "<h1>Usuário já cadastrado</h1>"
+        if nome and cpf and email and dados:
+            adicionar_usuario(nome, cpf, email, senha)
+            return redirect(url_for("relatorios"))
+        else:
+            return "<h1>Por favor preencha todos os campos</h1>"
+    return render_template("tela_cadastro.html")
