c74a8a003a3e9bd8e75927344bbd920ecc1906b9
Darlan Berg <164627933+Darlan-Berg@users.noreply.github.com>
2024-10-12 10:01:36 -0300

    Update index.js

--- static/index.js
+++ static/index.js
@@ -43,15 +43,24 @@ function submitForm(event) {
     const dataValidade = document.getElementById('data-validade').value;
 
     // Cria um novo cartão com as informações
-    const containerCartoes = document.getElementById('container-cartoes');
+    const containerCartoes = document.getElementById('container-cards-produtos');
     const cardDiv = document.createElement('div');
+    cardDiv.classList.add(`card`);
     cardDiv.innerHTML = `
-        <div class="card">
-            <span>Nome: ${nome}</span>
-            <span>Marca: ${marca}</span>
-            <span>Valor da Compra: ${valorCompra}</span>
-            <span>Quantidade: ${quantidade}</span>
-            <span>Data de Validade: ${dataValidade}</span>
+        <div>
+        <span>Nome: </span> <span class="nome">${nome}</span>
+        </div>
+        <div>
+            <span>Marca: </span> <span class="marca">${marca}</span>
+        </div>
+        <div>
+            <span>Valor da Compra: </span> <span class="valor">${valorCompra}</span>
+        </div>
+        <div>
+            <span>Quantidade: </span> <span class="quantidade">${quantidade}</span>
+        </div>
+        <div>
+            <span>Data de Validade: </span> <span class="data-validade">${dataValidade}</span>
         </div>
     `;
     containerCartoes.appendChild(cardDiv);
@@ -59,4 +68,23 @@ function submitForm(event) {
     // Limpa o formulário e fecha a segunda modal
     document.getElementById('infoForm').reset();
     fecharModal(`.modal`, `#backdrop2`);
+}
+
+function coletarProdutosDoModal () {
+    const dataCompra = document.querySelector(`#data`);
+    const id = 12345; // gerar id para cada compra
+
+    let produtos = [];
+    const conteudoDoModal = document.querySelectorAll(`.card`);
+    conteudoDoModal.forEach(card => {
+        let produto = {
+            nome: card.querySelector(`.nome`).innerHTML,
+            nome: card.querySelector(`.marca`).innerHTML,
+            nome: card.querySelector(`.valor`).innerHTML,
+            nome: card.querySelector(`.quantidade`).innerHTML,
+            nome: card.querySelector(`.data-validade`).innerHTML
+        };
+        produtos.push(produto);
+    })
+    return produtos;
 }
